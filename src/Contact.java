56c5d30286a35bb81ca5e2367f1d5eb904267418
Matthew Fischer <mattfisc@gmail.com>
2021-06-01 00:43:43 -0700

    new code ware challenge
    starting phone challenge.  created contact class.

---
+++ src/Contact.java
@@ -0,0 +1,11 @@
+public class Contact{
+    int number;
+    String name;
+    String address;
+
+    public Contact(int number, String name, String address){
+        this.number = number;
+        this.name = name;
+        this.address = address;
+    }
+}
---
+++ src/PhoneDir.java
@@ -0,0 +1,24 @@
+// Phone Directory
+
+public class PhoneDir {
+
+
+    // (full data base , number to look for)
+    // return object found
+    public static  String phone(String strng, String num) {
+        // parse string
+
+
+        // find num
+
+        // return num object
+
+        // excepts
+        // too many people to number
+        // number not found
+
+
+        return "";
+    }
+}
+
